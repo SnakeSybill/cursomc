712f1d92747969836a4eeaa9cc01db5c55d6c1af
Igor <igorgonribsilva@gmail.com>
2019-09-08 16:25:32 -0300

    Delete with data integrity exception

--- src/main/java/com/igorgrs/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/igorgrs/cursomc/resources/CategoriaResource.java
@@ -4,6 +4,7 @@ import java.net.URI;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -42,6 +43,13 @@ public class CategoriaResource {
 	public ResponseEntity<Void> update(@RequestBody Categoria categoria, @PathVariable Integer id) {
 		categoria.setId(id);
 		categoria = categoriaService.update(categoria);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable(value = "id") Integer id) {
+		
+		categoriaService.delete(id);
 		return ResponseEntity.noContent().build();
 	}
 }
--- src/main/java/com/igorgrs/cursomc/services/CategoriaService.java
+++ src/main/java/com/igorgrs/cursomc/services/CategoriaService.java
@@ -3,9 +3,11 @@ package com.igorgrs.cursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.igorgrs.cursomc.domain.Categoria;
+import com.igorgrs.cursomc.exceptions.DataIntegrityException;
 import com.igorgrs.cursomc.exceptions.ObjectNotFoundException;
 import com.igorgrs.cursomc.repository.CategoriaRepository;
 
@@ -29,9 +31,18 @@ public class CategoriaService {
 		obj.setId(null);
 		return objRepository.save(obj);
 	}
-	
+
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return objRepository.save(obj);
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			objRepository.deleteById(id);
+		} catch (DataIntegrityViolationException ex) {
+			throw new DataIntegrityException("This record has children, unable to delete it.");
+		}
 	}
 }
